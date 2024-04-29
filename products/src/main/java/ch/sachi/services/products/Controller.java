b2ad9e9d9d3608d2f0392883e56fe8db7d82fa15
Patrick Zbinden <patrick+dev@zbinden-online.ch>
2024-04-29 16:09:37 +0200

    add trace_id to log

--- products/src/main/java/ch/sachi/services/products/Controller.java
+++ products/src/main/java/ch/sachi/services/products/Controller.java
@@ -2,6 +2,8 @@ package ch.sachi.services.products;
 
 import ch.sachi.services.products.persistence.Product;
 import ch.sachi.services.products.persistence.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,10 @@ public class Controller {
 
     @GetMapping("products")
     public List<ProductDto> getAllProducts() {
+        final Logger logger = LoggerFactory.getLogger(getClass());
+        logger.info("Start getting products");
         final List<Product> products = productRepo.getAllProducts();
+        logger.info("We have {} products", products.size());
         return products.stream()
                 .map(p -> new ProductDto(p.getId(), p.getName()))
                 .toList();
