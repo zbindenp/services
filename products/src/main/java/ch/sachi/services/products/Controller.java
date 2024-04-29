82393e1376e786e89fcf1ddbbf253aaa8e8dd21a
Patrick Zbinden <patrick+dev@zbinden-online.ch>
2024-04-29 09:57:09 +0200

    Initial commit

---
+++ products/src/main/java/ch/sachi/services/products/Controller.java
@@ -0,0 +1,13 @@
+package ch.sachi.services.products;
+
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
