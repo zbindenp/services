0e23345470431d94ec76551f1dfa558d0d488315
Patrick Zbinden <patrick.zbinden@bison-group.com>
2024-04-30 14:27:53 +0200

    refactored

--- customers/src/main/java/ch/sachi/services/customers/CustomerDto.java
+++ customers/src/main/java/ch/sachi/services/customers/CustomerDto.java
@@ -1,19 +1,4 @@
 package ch.sachi.services.customers;
 
-public class CustomerDto {
-    private final Long id;
-    private final String name;
-
-    public CustomerDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getId() {
-        return id;
-    }
+public record CustomerDto(Long id, String name) {
 }
--- products/src/main/java/ch/sachi/services/products/ProductDto.java
+++ products/src/main/java/ch/sachi/services/products/ProductDto.java
@@ -1,22 +1,4 @@
 package ch.sachi.services.products;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-public class ProductDto {
-    private final Long id;
-    private final String name;
-
-    public ProductDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getId() {
-        return id;
-    }
+public record ProductDto(Long id, String name) {
 }
