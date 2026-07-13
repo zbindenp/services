763ed03af00ad0c96419f8f056fe36bc7ce1f44f
Patrick Zbinden <patrick.zbinden@bison-group.com>
2026-07-13 16:15:53 +0200

    use a nicer services.sh

--- redeploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
- kubectl delete -f apps/kube_apps.yaml
- kubectl apply -f apps/kube_apps.yaml
--- services.sh
+++ services.sh
@@ -14,10 +14,44 @@ build() {
 }
 
 deploy() {
-  project=$(gum choose "products" "customers" "categories" "main")
+  project="${1:-$(gum choose "products" "customers" "categories" "main")}"
   export NAME=$project
   export DEPLOY=$(date '+%Y%m%d:%H%M%S')
   envsubst < apps/service.yaml | kubectl apply -f -
   unset NAME
   unset DEPLOY
+}
+
+services() {
+  projects="${2:-$(gum choose --no-limit "products" "customers" "categories" "main")}"
+  case "$1" in
+  build)
+    for project in $projects; do
+      echo "Building $project"
+      ./gradlew :$project:bootJar
+    done
+    ;;
+  push)
+    for project in $projects; do
+      echo "Pushing image $project"
+      docker build -t "$IMAGE_ROOT_PATH/$project:latest" ./$project
+      docker push "$IMAGE_ROOT_PATH/$project:latest"
+    done
+    ;;
+  deploy)
+    if [[ -z "${SA_PASSWORD}" ]]; then
+      echo "env SA_PASSWORD must be set"
+      return 1
+    fi
+    for project in $projects; do
+      echo "Deploying $project"
+      export DEPLOY=$(date '+%Y%m%d:%H%M%S')
+      envsubst < "$project/service.yaml" | kubectl apply -f -
+      unset DEPLOY
+    done
+    ;;
+    * )
+      echo "services [build|push\deploy]"
+  esac
+
 }
