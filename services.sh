010ac036d0729220947a7c0251038d177c6c84bf
Patrick Zbinden <patrick.zbinden@bison-group.com>
2026-07-20 11:57:19 +0200

    Fix scripts

--- services.sh
+++ services.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(dirname "$0")
 
 select_projects() {
   if [[ "${1}" ]]; then
@@ -11,25 +10,27 @@ select_projects() {
 }
 
 services() {
-  source "${SCRIPT_DIR}/.env"
+
+  source "./.env"
   case "$1" in
   init)
     echo "select JAVA_HOME"
     JAVA_HOME=$(gum file ${JAVA_HOME}/.. --directory)
     echo "Set Docker Registry"
     DOCKER_REGISTRY=$(gum input --placeholder "JAVA_HOME" --value="${DOCKER_REGISTRY:-"localhost:5000"}")
-    echo "JAVA_HOME=${JAVA_HOME}" > "${SCRIPT_DIR}/.env"
-    echo "DOCKER_REGISTRY=${DOCKER_REGISTRY}" >> "${SCRIPT_DIR}/.env"
+    echo "Writing env to [./.env]"
+    echo "JAVA_HOME=${JAVA_HOME}" > "./.env"
+    echo "DOCKER_REGISTRY=${DOCKER_REGISTRY}" >> "./.env"
     ;;
   build)
-    projects=($(select_projects "$2"))
+    projects=$(select_projects "$2")
     for project in $projects; do
       echo "Building $project"
       ./gradlew :$project:bootJar
     done
     ;;
   push)
-    projects=($(select_projects "$2"))
+    projects=$(select_projects "$2")
     for project in $projects; do
       echo "Pushing image $project"
       image="${DOCKER_REGISTRY}/services/${project}:latest"
@@ -39,7 +40,7 @@ services() {
     ;;
   deploy)
     echo "helm must be installed"
-    pushd "${SCRIPT_DIR}/deploy"
+    pushd "./deploy"
       helm upgrade -i services . -n services --create-namespace
     popd
     ;;
